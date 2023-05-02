00ddd954d9235a510b257e3a9c13f5477517b602
Anselme Guillaume <anselmeguillaume@orange.fr>
2023-05-02 10:30:57 +0200

    debut ajout  alert

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AircraftsState, AircraftStateEnum } from 'src/app/ngrx/aircrafts.state';
 
 @Component({
   selector: 'app-alert',
   templateUrl: './alert.component.html',
   styleUrls: ['./alert.component.css']
 })
-export class AlertComponent {
+export class AlertComponent implements OnInit{
+  aircraftsState$:Observable<AircraftsState> | null = null;
+  readonly aircraftsStateEnum = AircraftStateEnum;
 
+  constructor(private store:Store<any>) {}
+  
+  ngOnInit(): void {
+    this.aircraftsState$ = this.store.pipe(
+      map((state) => state.airbusState)
+    );
+  }
 }
